c03393085ea630034ff1a1eec0aa52c80526c13e
Manojsekhar <manojsekhar19@gmail.com>
2025-12-28 19:24:47 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "local_file" "my_pet" {
-  filename = "pets.txt"
-  content  = "i hate pets!"
-}
-resource "random_pet" "my_pet" {
-  prefix    = "MR"
-  separator = "."
-  length    = 1
-}
